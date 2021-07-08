96f2f03073dbc3f09f926a17c5fa75ded2e310d3
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-07-08 17:21:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import os
 import telegram
 import telegram.ext 
+import logging
 
+# Логи
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+# Токен
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-UPDATER = telegram.ext.Updater(token=BOT_TOKEN, use_context=True)
+# Всякая фигня
+updater = telegram.ext.Updater(token=BOT_TOKEN, use_context=True)
+dispatcher = UPDATER.dispatcher
 
+# Устанавливаем какие-то держатели
+from telegram.ext import CommandHandler
+
+start_handler = CommandHandler('start', start)
+
+# Устанавливаем какие-то держатели окончательно
+dispatcher.add_handler(start_handler)
+
+# Крутая функция
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Этот мир не дорос до крутых вещей!")
+
+# Запускаем бота
+updater.start_polling()
