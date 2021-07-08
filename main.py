630d0bbaff6b61788735115b53fa3c0ef3b0b0f7
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-07-08 17:07:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
-import telegram.ext as tg
+import telegram as tg1
+import telegram.ext as tg2
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-UPDATER = tg.Updater(token=BOT_TOKEN, use_context=True)
+UPDATER = tg2.Updater(token=BOT_TOKEN, use_context=True)
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Этот мир не дорос до крутых вещей!")
