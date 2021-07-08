d88f843df287b718e42c7b6dc79fde874e0a7613
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-07-08 17:29:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ updater = telegram.ext.Updater(token=BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 ## Крутые функции
-def start(update, context):
+def cmd_start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Вы не готовы к крутости такого уровня...")
+
+def cmd_openmc(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Вы открыли мифический сундук")
 
 ## Устанавливаем какие-то держатели
 from telegram.ext import CommandHandler
 
-start_handler = CommandHandler('start', start)
+start_handler = CommandHandler('start', cmd_start)
+openmc_handler = CommandHandler('openmc', cmd_openmc)
 
 # Устанавливаем какие-то держатели окончательно
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(openmc_handler)
 
 ## Запускаем бота
 updater.start_polling()
