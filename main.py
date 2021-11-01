eeda538782419a6cdcc09d3ca77757abbbb605d1
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-11-02 03:08:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ CURSOR = DATABASE.cursor()
 
 ## Крутые функции
 def cmd_start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Вы не готовы к крутости такого уровня...")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Я чайный бот!")
 
-def cmd_selldata(update, context):
+def cmd_createshroom(update, context):
     userId = update.message.from_user.id;
     userName = update.message.from_user.first_name;
     
@@ -26,33 +26,33 @@ def cmd_selldata(update, context):
     fetch = CURSOR.fetchone()
     
     if fetch == None:
-        CURSOR.execute ("INSERT INTO users (userid, name, balance) VALUES ("+str(userId)+", '"+userName+"', 3)")
+        CURSOR.execute ("INSERT INTO users (userid, name, balance) VALUES ("+str(userId)+", '"+userName+"', 1)")
         DATABASE.commit()
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Вы продали свои данные за три фальшивых рубля")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="")
     
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Вы уже продавали свои данные")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="У Вас уже есть чайный гриб, имейте совесть")
         print(fetch)
 
-def cmd_balance(update, context):
+def cmd_checkshroom(update, context):
     userId = update.message.from_user.id;
     
     CURSOR.execute ("SELECT balance FROM users WHERE userid="+str(userId))
     fetch = CURSOR.fetchone()
     
     if fetch == None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Вы ещё не числитесь в наших базах")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="У вас ещё нет чайного гриба")
     
     else:
         bmsg_balance = str(fetch[0])
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Ваш баланс: "+bmsg_balance+" фальшивых рубля(ей)")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Размер чайного гриба: "+bmsg_balance+" мм")
     
 ## Устанавливаем какие-то держатели
 from telegram.ext import CommandHandler
 
 start_handler = CommandHandler('start', cmd_start)
-selldata_handler = CommandHandler('selldata', cmd_selldata)
-balance_handler = CommandHandler('balance', cmd_balance)
+createshroom_handler = CommandHandler('createshroom', cmd_createshroom)
+checkshroom_handler = CommandHandler('checkshroom', cmd_checkshroom)
 
 # Устанавливаем какие-то держатели окончательно
 dispatcher.add_handler(start_handler)
