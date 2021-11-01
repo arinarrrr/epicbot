ba2c05ff3f8fde4aa546c4a70a4ae7d2e906a6bd
IDCallName <73123314+IDCallName@users.noreply.github.com>
2021-11-02 03:54:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import telegram
 import telegram.ext
 import psycopg2
+import threading
+import random
+from time import sleep
 
 ## Токен
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
@@ -14,10 +17,63 @@ dispatcher = updater.dispatcher
 DATABASE = psycopg2.connect(dbname='dc9mv72g5rq199', user='expfuoggsoeeqp', password='8be9b873d53b0b38ba8fb3b7a0274db21e934813af12ecf4ed0bdee244422707', host='ec2-54-220-170-192.eu-west-1.compute.amazonaws.com')
 CURSOR = DATABASE.cursor()
 
+## Таймер
+RandomCooldown = {}
+
+## Циклы гриба
+def shroom_update_cycle():
+    sleep(900)
+    CURSOR.execute ("SELECT * FROM users")
+    fetch = CURSOR.fetchone()
+    
+    while fetch != None:
+        userId = fetch[0]
+        size = fetch[2]
+        
+        size += 1
+        
+        CURSOR.execute (f"UPDATE users SET balance = {size} WHERE userid = {userId}")
+        DATABASE.commit()
+    
+    for timer in RandomCooldown:
+        RandomCooldown[timer] -= 15
+        
+        if RandomCooldown[timer] <= 0:
+            del RandomCooldown[timer]
+    
 ## Крутые функции
 def cmd_start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Я чайный бот!")
 
+def cmd_random(update, context):
+    userId = update.message.from_user.id;
+    
+    if userId in RandomCooldown:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Полегче-полегче! Следующий рандом вам будет доступен примерно через {RandomCooldown[userId]}")
+        
+    else:
+        CURSOR.execute ("SELECT balance FROM users WHERE userid="+str(userId))
+        fetch = CURSOR.fetchone()
+
+        if fetch == None:
+            context.bot.send_message(chat_id=update.effective_chat.id, text="У вас нет чайного гриба!")
+
+        else:
+            size = fetch[0]
+            deltaSize = int(random.randrange(-10, 12))
+
+            CURSOR.execute (f"UPDATE users SET balance = {size+deltaSize} WHERE userid = {userId}")
+            DATABASE.commit()
+
+            RandomCooldown.update{userId: 180}
+
+            if deltaSize > 0:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"Ого! Ваш чайный гриб увеличился на {deltaSize} мм!")
+            elif deltaSize == 0:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"Кринж! Ваш чайный гриб не изменился")
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=f"Жесть! Ваш чайный гриб уменьшился на {-deltaSize} мм!")
+                       
 def cmd_createshroom(update, context):
     userId = update.message.from_user.id;
     userName = update.message.from_user.first_name;
@@ -28,7 +84,7 @@ def cmd_createshroom(update, context):
     if fetch == None:
         CURSOR.execute ("INSERT INTO users (userid, name, balance) VALUES ("+str(userId)+", '"+userName+"', 1)")
         DATABASE.commit()
-        context.bot.send_message(chat_id=update.effective_chat.id, text="")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Чайный гриб создан")
     
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text="У Вас уже есть чайный гриб, имейте совесть")
@@ -53,11 +109,16 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', cmd_start)
 createshroom_handler = CommandHandler('createshroom', cmd_createshroom)
 checkshroom_handler = CommandHandler('checkshroom', cmd_checkshroom)
+random_handler = CommandHandler('random', cmd_random)
 
 # Устанавливаем какие-то держатели окончательно
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(createshroom_handler)
 dispatcher.add_handler(checkshroom_handler)
+dispatcher.add_handler(random_handler)
+
+# Запускаем автообновлялку
+threading.Thread(target=shroom_update_cycle).start()
 
 ## Запускаем бота
 updater.start_polling()
