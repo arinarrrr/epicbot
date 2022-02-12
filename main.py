fc87e2f670478bf4784e4e86ad16a06cad77c432
arinarrrr <99396512+arinarrrr@users.noreply.github.com>
2022-02-12 14:55:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def msg_analysis(update, context):
     elif(update.effective_message.text == "Нет"):
         context.bot.send_message(chat_id=update.effective_chat.id, text="Хорошо, что на счет лекции про основные философские вопросы на Арзамас?")
     if(update.effective_message.text == "Да"):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Отлично, вот ссылка: ---")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Отлично, вот ссылка: https://arzamas.academy/materials/1771")
         context.bot.send_message(chat_id=update.effective_chat.id, text="Мы можем предложить тебе пару развлечений на вечер. Как тебе идея виртуальной прогулки по Эрмитажу? Ответь да/нет")
         
         
